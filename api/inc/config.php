129c24de2b63cd9b2f7fd01dc9815ae3eef7640b
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-09 15:42:16 -0300

    .

---
+++ api/inc/config.php
@@ -0,0 +1,7 @@
+<?php
+
+    define ('DB_SERVER', 'localhost');
+    define ('DB_NAME', 'loja_api');
+    define ('DB_CHARSET', 'utf8');
+    define ('DB_USERNAME','user_loja_api');
+    define ('DB_PASSWORD','VIyubiLehI4Ajoqu2ak2zIc71uv374');
